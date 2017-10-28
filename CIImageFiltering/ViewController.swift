fb7a06c9719d3da4da141f0c3b0acb9533272ad0
Mark Wilkinson <markawil@yahoo.com>
2017-10-27 23:23:13 -0500

    Initial Commit

---
+++ CIImageFiltering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIImageFiltering
+//
+//  Created by Mark Wilkinson on 10/27/17.
+//  Copyright © 2017 Mark Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
